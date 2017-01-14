6673829e5a7b13080d5d198432e76938540ad008
Greenwicher <weizhiliu2009@gmail.com>
2017-01-14 22:27:04 +0800

    implement the normal sampling

--- PyPRS/rule/sampleMethod.py
+++ PyPRS/rule/sampleMethod.py
@@ -9,6 +9,7 @@
 
 import numpy as np
 from .. import utils
+import random
 
 def uniform(leaf,n):
     """draw n uniform distributed samples from this leaf
@@ -34,3 +35,28 @@ def discretize(leaf, samples):
     LB, UB, discreteLevel = leaf.problem.lb, leaf.problem.ub, leaf.problem.discreteLevel
     discretizedSamples = utils.discretize(samples, LB, UB, discreteLevel)
     return discretizedSamples
+    
+def normal(leaf, n):
+    """draw n uniform distributed samples from this leaf
+    Args:
+        leaf: A class Tree() representing the leaf node region
+        n: An integer representing sampling size
+    Returns:
+        leaf: This leaf
+        samples: An n * dimX array representing the uniformlly samples     
+    """
+    visitedPoints = leaf.pool
+    paretoSet = utils.identifyParetoSet(visitedPoints)        
+    samples = []
+    while len(samples) < n:
+        if paretoSet:
+            pivot = paretoSet[random.choice(paretoSet.keys())]
+            loc = pivot.x
+        else:
+            loc = (leaf.lb + leaf.ub) / 2
+        scale = (leaf.root.ub - leaf.root.lb) / leaf.problem.discreteLevel
+        point = discretize(leaf, np.random.normal(loc,scale))
+        if leaf.withinNode(point):
+            samples.append(point)
+    samples = np.array(samples)
+    return {'leaf':leaf, 'samples':samples} 
