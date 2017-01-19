97b5dae6edb4de38f973dd6c397505bd773ddb6a
Greenwicher <weizhiliu2009@gmail.com>
2017-01-19 10:35:47 +0800

    change the function argument types to extend the use

--- PyPRS/performance.py
+++ PyPRS/performance.py
@@ -13,21 +13,14 @@ from . import utils
 import numpy as np
 
 
-def calHyperVolume(paretoSet,referencePoint):
+def calHyperVolume(front, referencePoint):
     """ calculate the hypervolume value
     Args:
-        paretoSet: A dictionary of class Point() representing the Pareto set
+        front: A list of "Pareto" front objectives value
         referencePoint: A numpy array representing the reference point
     Returns:
         hyperVolume: A doulbe representing the hypervolume value
     """
-    front = []
-    for k in paretoSet:
-        front.append(paretoSet[k].trueMean)
-    # use hv.py
-#    HV = hv.HyperVolume(referencePoint)
-#    hyperVolume = HV.compute(front)
-    # use PyGMO
     try:
         HV = PyGMO.hypervolume(front)
         hyperVolume = HV.compute(r=list(referencePoint))
@@ -39,18 +32,18 @@ def calTrueParetoProportition(paretoSet, trueParetoSet):
     """ calculate the proportion of true Pareto solution given the estimated 
         Pareto set and true Pareto set
     Args:
-        paretoSet: A dictionary of estimated Pareto set with class Point()
-        trueParetoSet: A dictionary of true Pareto set with class Point()
+        paretoSet: A list of decision vector of estimated Pareto set
+        trueParetoSet: A list of decision vector of true Pareto set
     Returns:
         trueParetoProportition: A double indicating the proportion of true 
         Pareto solution given the estimated Pareto set and true Pareto set
     """
     paretoSetKey = set()
     trueParetoSetKey = set()
-    for k in paretoSet:
-        paretoSetKey.add(utils.generateKey(paretoSet[k].x))
-    for k in trueParetoSet:
-        trueParetoSetKey.add(utils.generateKey(trueParetoSet[k].x))
+    for x in paretoSet:
+        paretoSetKey.add(utils.generateKey(x))
+    for x in trueParetoSet:
+        trueParetoSetKey.add(utils.generateKey(x))
     trueParetoProportition = len(paretoSetKey & trueParetoSetKey) / len(trueParetoSetKey)
     return trueParetoProportition
     
@@ -58,18 +51,16 @@ def calHausdorffDistance(paretoSet, trueParetoSet):
     """ calculate Hausdorff distance (see https://en.wikipedia.org/wiki/Hausdorff_distance)
         between the estimated Pareto set and true Pareto set
     Args:
-        paretoSet: A dictionary of estimated Pareto set with class Point()
-        trueParetoSet: A dictionary of true Pareto set with class Point()
+        paretoSet: A list of decision vector of estimated Pareto set
+        trueParetoSet: A list of decision vector of true Pareto set
     Returns:
         HausdorffDistance: A double indicating Hausdorff distance
     """    
-    paretoSetList = [paretoSet[k].x for k in paretoSet]
-    trueParetoSetList = [trueParetoSet[k].x for k in trueParetoSet]
-    m, n = len(paretoSetList), len(trueParetoSetList)
+    m, n = len(paretoSet), len(trueParetoSet)
     d = [[np.nan] * n for _ in range(m)]
     for i in range(m):
         for j in range(n):
-            d[i][j] = np.linalg.norm(paretoSetList[i] - trueParetoSetList[j])
+            d[i][j] = np.linalg.norm(paretoSet[i] - trueParetoSet[j])
     d = np.array(d)
     rowDistance, colDistance = [], []
     for i in range(m):
