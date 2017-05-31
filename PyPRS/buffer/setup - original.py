e6a9ab3147814437ce249ca3c3212b2d02ec34d9
Greenwicher <weizhiliu2009@gmail.com>
2017-05-31 16:08:43 +0800

    change the directiory

--- PyPRS/buffer/setup - original.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('cutils',
-                    sources = ['cutils.cpp'])
-
-setup (name = 'PyNP',
-       version = '1.0',
-       description = 'Python Implementation of Nested Partition Algorithm',
-       ext_modules = [module1])
